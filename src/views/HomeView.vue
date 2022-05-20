9eb56c38be3f8d3becd5d9329d7fd0e92c022067
Lorenzo <69311874+Lorenzo0111@users.noreply.github.com>
2022-05-20 20:13:28 +0200

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <div class="text-center relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
+      <h1 class="font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-orange-400 to-red-500">The new era of minecraft development</h1>
+      <p class="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">RocketPlugins is a freelancing team ready to empower your service or activity with out best custom products made for you and your necessities.</p>
+    </div>
+  
+</main>
+</template>
