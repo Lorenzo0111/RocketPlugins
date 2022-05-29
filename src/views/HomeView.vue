fbc8bf3558c6215436884610bfbdca28d0d3ed94
Lorenzo <69311874+Lorenzo0111@users.noreply.github.com>
2022-05-29 17:04:50 +0200

    Various changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,11 @@
 <template>
   <main>
     <div class="text-center relative max-w-5xl mx-auto pt-20 sm:pt-24 lg:pt-32">
-      <h1 class="font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-orange-400 to-red-500">The new era of minecraft development</h1>
-      <p class="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">RocketPlugins is a freelancing team ready to empower your service or activity with out best custom products made for you and your necessities.</p>
+        <div class="md:flex">
+            <img src="src/assets/icon.png" width="300" class="m-auto">
+            <h1 class="md:m-auto font-extrabold text-transparent text-5xl bg-clip-text bg-gradient-to-r from-orange-400 to-red-500">The new era of minecraft development</h1>
+        </div>
+        <p class="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400">RocketPlugins is a freelancing team ready to empower your service or activity with our best custom products made for you and your necessities.</p>
     </div>
   
 </main>
